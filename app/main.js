2c5b9b212ae09d3169cd91ce579a02da0a0561a5
Junior Martins <amjr.box@gmail.com>
2021-08-25 22:26:47 -0300

    Inicío da estruturação do layout

--- app/main.js
+++ app/main.js
@@ -1,6 +1,9 @@
-import './style.css'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
+/*
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+*/
